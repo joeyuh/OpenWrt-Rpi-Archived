32fca92e401cc8d34e703335d35f665d765a18c2
Joe Yu <grandpajoe278@gmail.com>
2021-07-28 19:32:37 +0800

    no fancy shell

--- scripts/preset-terminal-tools.sh
+++ scripts/preset-terminal-tools.sh
@@ -8,19 +8,19 @@
 # Blog: https://mlapp.cn
 #=================================================
 
-mkdir -p files/root
-pushd files/root
+# mkdir -p files/root
+# pushd files/root
 
 ## Install oh-my-zsh
 # Clone oh-my-zsh repository
-git clone https://github.com/robbyrussell/oh-my-zsh ./.oh-my-zsh
+# git clone https://github.com/robbyrussell/oh-my-zsh ./.oh-my-zsh
 
 # Install extra plugins
-git clone https://github.com/zsh-users/zsh-autosuggestions ./.oh-my-zsh/custom/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ./.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-completions ./.oh-my-zsh/custom/plugins/zsh-completions
+# git clone https://github.com/zsh-users/zsh-autosuggestions ./.oh-my-zsh/custom/plugins/zsh-autosuggestions
+# git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ./.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+# git clone https://github.com/zsh-users/zsh-completions ./.oh-my-zsh/custom/plugins/zsh-completions
 
 # Get .zshrc dotfile
-cp ../../../data/zsh/.zshrc .
+# cp ../../../data/zsh/.zshrc .
 
-popd
+# popd
